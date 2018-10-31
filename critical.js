94b1109b7645c6306630bf255835d05a0124d0a3
smakhl <SMakhlonov@slb.com>
2018-10-31 13:00:23 +0300

    update critical config

--- critical.js
+++ critical.js
@@ -1,10 +1,12 @@
-var critical = require('critical');
+var critical = require("critical");
 
 critical.generate({
-    inline: true,
-    base: 'src2/',
-    src: 'index.html',
-    dest: 'index-critical.html',
-    minify: true,
-    extract: true
+  inline: true,
+  base: "src2/",
+  src: "base.html",
+  dest: "index.html",
+  minify: true,
+  extract: true,
+  include: [".link", ".link:active", ".link:visited"]
+  // css: ['src2/styles.css', 'src2/fonts/pt_sans.css',],
 });
